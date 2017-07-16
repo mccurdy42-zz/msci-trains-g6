4449aab73d941cf73521aad34bd0ba0deb95baf7
Pooja0625 <poojapatel0625@gmail.com>
2017-07-16 17:32:56 -0400

    Update SQL - query 2.php

--- SQL - query 2.php
+++ SQL - query 2.php
@@ -1,3 +1,5 @@
+Operating Company is trying to find the employee ID's of their employees who have been working for 10 or more years. Here are their results!
+
 $sql = " select E.`e_id`, E.`yrs_exp`" 
 . " FROM employee E, `operating_comp` C" 
 . " WHERE E.`c_id` = ?"  
